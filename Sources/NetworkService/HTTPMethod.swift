3134735e24bb5f41e1ad3e8011989ab244ea516b
Ivan <ivankirpichonok@gmail.com>
2023-06-21 09:01:54 +0300

    Add HTTPMethod file

---
+++ Sources/NetworkService/HTTPMethod.swift
@@ -0,0 +1,11 @@
+/// The HTTP request method.
+public enum HTTPMethod: String {
+    /// The "GET" request.
+    case get = "GET"
+
+    /// The "POST" request.
+    case post = "POST"
+
+    /// The "PUT" request.
+    case put = "PUT"
+}
