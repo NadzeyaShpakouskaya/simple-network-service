e951f83c29ba95a6430aa8f9736d3b4bf404af81
Ivan <ivankirpichonok@gmail.com>
2023-06-21 09:13:05 +0300

    Add Request file,  add documentation and init

---
+++ Sources/NetworkService/Request.swift
@@ -0,0 +1,39 @@
+import Foundation
+
+/// Encapsulates an HTTP request with the necessary components.
+public struct Request {
+    public typealias HTTPHeaderKey = String
+    public typealias HTTPHeaderValue = String
+    public typealias HTTPHeaders = [HTTPHeaderKey: HTTPHeaderValue]
+
+    /// Creates a request with the provided parameters.
+    ///
+    /// - Parameters:
+    ///    - URI: The URL of the request.
+    ///    - requestMethod: The HTTP request method.
+    ///    - headers: HTTP header fields for the request.
+    ///    - body: The data sent as the message body of the request.
+    public init(
+        URI: URL,
+        requestMethod: HTTPMethod,
+        headers: Request.HTTPHeaders,
+        body: String? = nil
+    ) {
+        self.URI = URI
+        self.requestMethod = requestMethod
+        self.headers = headers
+        self.body = body
+    }
+
+    /// The URL of the request.
+    public let URI: URL
+
+    /// The HTTP request method.
+    public let requestMethod: HTTPMethod
+
+    /// HTTP header fields for the request.
+    public let headers: HTTPHeaders
+
+    /// The data sent as the message body of the request.
+    public let body: String?
+}
