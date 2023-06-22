ecaf3e099359ae9d28ed56f4373c704e644700be
Julya Kmita <>
2023-06-22 17:20:00 +0800

    Fixed cases description and removed some cases

--- Sources/NetworkService/NetworkError.swift
+++ Sources/NetworkService/NetworkError.swift
@@ -8,30 +8,29 @@ public enum NetworkError: Error, CaseIterable {
     /// Indicates that the server cannot handle the request (because it is overloaded or down for maintenance).
     case serverUnavailable
     
-    /// Indicates that the server cannot process the request due to a client error (e.g., malformed request syntax, size too large, invalid request message framing, or deceptive request routing).
+    /// Indicates that the server cannot process the request due to a client error
+    /// (e.g., malformed request syntax, size too large, invalid request message framing, or deceptive request routing).
     case badRequest
     
     /// Indicates that the request to connect and log on to an FTP server could not be completed
     /// because the supplied password is incorrect.
     case passwordError
-
+    
     /// Indicates that the request to connect to and log on to an FTP server failed.
     case loginError
-
-    /// Indicates that the server timed out waiting for the request.
-    /// The client did not produce a request within the time that the server was prepared to wait.
-    /// The client may repeat the request without modifications at any later time.
+    
+    /// Indicates that the server stopped waiting for the request because the client took too long to produce a request.
     case requestTimeOut
     
     /// Indicates that the client's session has expired and must log in again.
-    case loginTimeOut
+    case sessionExpired
     
     /// Indicates that the Uniform Resource Locator is invalid.
     case invalidURL
-
+    
+    /// Indicates that the response was successful, but no data comes back.
+    case missingData
+    
     /// Not specified error.
     case unknownError
-    
-    /// Indicates that decoding of json responses was failed.
-    case decodingError
 }
