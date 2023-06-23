c371bcde4c5b79a0de8acef2909baadac3647780
marinkman <marinkman@gmail.com>
2023-06-23 13:15:39 +0300

    correct `errorDescription`

--- Sources/NetworkService/NetworkError/NetworkError+LocalizedError.swift
+++ Sources/NetworkService/NetworkError/NetworkError+LocalizedError.swift
@@ -9,19 +9,19 @@ extension NetworkError: LocalizedError {
         case .serverUnavailable:
             return "The server is currently unavailable. Please try again later."
         case .badRequest:
-            return "badRequest"
+            return "The server cannot process the request because an error was found in the request. Please check the request and try again."
         case .authenticationError:
-            return "authenticationError"
+            return "The authentication is required for access."
         case .requestTimeOut:
-            return "requestTimeOut"
+            return "The server timed out waiting for the request. Please try again."
         case .sessionExpired:
-            return "sessionExpired"
+            return "The session has expired. Please try again."
         case .invalidURL:
-            return "invalidURL"
+            return "The server cannot process the request due to an invalid URL."
         case .missingData:
-            return "missingData"
+            return "The server successfully processed the request, but there is no data for the request."
         case .unknownError:
-            return "Unknown error."
+            return "Unknown error. Please try again"
         }
     }
 }
