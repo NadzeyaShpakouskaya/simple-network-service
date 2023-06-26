699c1710e924490556e0bb87c49513e5edadcd46
Ivan <ivankirpichonok@gmail.com>
2023-06-26 08:59:17 +0300

    Rename to HTTPRequestInitTests: add tests for init.

---
+++ Tests/NetworkServiceTests/HTTPRequestTests/HTTPRequestInitTests.swift
@@ -0,0 +1,50 @@
+import NetworkService
+import XCTest
+
+final class HTTPRequestInitTests: XCTestCase {
+    func testCreateNewInstanceWithExpectedPropertyValues() {
+        // Given
+        let url = URL(string: "https://www.example.com")!
+        let httpMethod = HTTPMethod.post
+        let body = ""
+
+        // When
+        let requestWithoutBody = HTTPRequest(
+            URI: url,
+            requestMethod: httpMethod
+        )
+        let requestWithBody = HTTPRequest(
+            URI: url,
+            requestMethod: httpMethod,
+            body: body
+        )
+
+        // Then
+        XCTAssertEqual(requestWithoutBody.URI, url)
+        XCTAssertEqual(requestWithoutBody.requestMethod, httpMethod)
+        XCTAssertEqual(requestWithoutBody.allHTTPHeaders, [:])
+        XCTAssertNil(requestWithoutBody.body)
+
+        XCTAssertEqual(requestWithBody.URI, url)
+        XCTAssertEqual(requestWithBody.requestMethod, httpMethod)
+        XCTAssertEqual(requestWithBody.allHTTPHeaders, [:])
+        XCTAssertEqual(requestWithBody.body, body)
+    }
+
+    func testNewInstanceHasNilBodyIfHTTPMethodIsGetAndBodyIsPassed() {
+        // Given
+        let url = URL(string: "https://www.example.com")!
+        let httpMethod = HTTPMethod.get
+        let body = ""
+
+        // When
+        let requestWithBody = HTTPRequest(
+            URI: url,
+            requestMethod: httpMethod,
+            body: body
+        )
+
+        // Then
+        XCTAssertNil(requestWithBody.body)
+    }
+}
