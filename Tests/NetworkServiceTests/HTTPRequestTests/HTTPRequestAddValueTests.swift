d3ed52cd5b82ceb97c91e06ea8030d42e806c2d9
Ivan <ivankirpichonok@gmail.com>
2023-06-26 09:41:43 +0300

    Add HTTPRequestAddValueTests: add file and tests

---
+++ Tests/NetworkServiceTests/HTTPRequestTests/HTTPRequestAddValueTests.swift
@@ -0,0 +1,82 @@
+import NetworkService
+import XCTest
+
+final class HTTPRequestAddValueTests: XCTestCase {
+    override func setUp() async throws {
+        try await super.setUp()
+        request = HTTPRequest(
+            URI: URL(string: "https://www.example.com")!,
+            requestMethod: .post
+        )
+    }
+
+    func testCreatesNewHeaderWhenAllHeadersIsEmpty() {
+        // Given
+        let headerName = "A"
+        let headerValue = "B"
+
+        // When
+        request.addValue(headerValue, for: headerName)
+
+        // Then
+        XCTAssertEqual(request.allHTTPHeaders, [headerName: headerValue])
+    }
+
+    func testAddsNewHeaderValueWhenTheHeaderValueIsEmptyString() {
+        // Given
+        let headerName = "A"
+        let headerValue = ""
+
+        // When
+        request.addValue(headerValue, for: headerName)
+
+        // Then
+        XCTAssertEqual(request.allHTTPHeaders, [headerName: headerValue])
+    }
+
+    func testAddsNewHeaderValueWhenTheHeaderValueIsSpace() {
+        // Given
+        let headerName = "A"
+        let headerValue = " "
+
+        // When
+        request.addValue(headerValue, for: headerName)
+
+        // Then
+        XCTAssertEqual(request.allHTTPHeaders, [headerName: headerValue])
+    }
+
+    func testAddsNewHeaderValueWhenAllHeadersContainSuchHeader() {
+        // Given
+        let headerName = "A"
+        let headerValueOne = "B"
+        let headerValueTwo = "C"
+        let expectedHeaderValue = "B,C"
+        request.addValue(headerValueOne, for: headerName)
+
+        // When
+        request.addValue(headerValueTwo, for: headerName)
+
+        // Then
+        XCTAssertEqual(request.allHTTPHeaders, [headerName: expectedHeaderValue])
+    }
+
+    func testAddsEmptyStringAndSpaceToExistingHeader() {
+        // Given
+        let headerName = "A"
+        let headerValueOne = " "
+        let headerValueTwo = ""
+        let expectedHeaderValue = " ,"
+
+        // When
+        request.addValue(headerValueOne, for: headerName)
+        request.addValue(headerValueTwo, for: headerName)
+
+        // Then
+        XCTAssertEqual(request.allHTTPHeaders, [headerName: expectedHeaderValue])
+    }
+
+    // MARK: - Private interface
+
+    private var request: HTTPRequest!
+}
