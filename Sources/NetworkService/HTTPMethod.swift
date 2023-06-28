3ba4d553840509c85b972f4218dc4e13a21b166f
Ivan <ivankirpichonok@gmail.com>
2023-06-28 15:18:39 +0300

    Move CaseIterable to NetworkService target.

--- Sources/NetworkService/HTTPMethod.swift
+++ Sources/NetworkService/HTTPMethod.swift
@@ -1,6 +1,6 @@
 /// The method to use in the HTTP requests.
 /// Indicates what actions to be performed by the remote host.
-public enum HTTPMethod: String {
+public enum HTTPMethod: String, CaseIterable {
     /// The `GET` method according to the [RFC 7231](https://datatracker.ietf.org/doc/html/rfc7231#section-4.3.1).
     case get = "GET"
 
