a12b184233aaccd8047c90cfad2bcae0fc337a1a
NadzeyaShpakouskaya <shpakovna@gmail.com>
2023-08-14 12:30:47 +0400

    Refactoring: swiftformat applyed

--- Sources/NetworkService/BodyEncoder/JSONBodyEncoder.swift
+++ Sources/NetworkService/BodyEncoder/JSONBodyEncoder.swift
@@ -4,20 +4,20 @@ import Foundation
 public struct JSONBodyEncoder: BodyEncoder {
     /**
      Encodes `BodyParameters` as the message body into the provided `URLRequest`.
-     
+
      - Parameters:
      - parameters: An optional `BodyParameters` object containing the data to be encoded.
      - request: An `inout` `URLRequest` object to which the encoded body will be assigned.
-     
+
      - Throws: An error of type `InternalError` if the encoding process fails.
      */
     public static func encode(_ parameters: BodyParameters?, into request: inout URLRequest) throws {
-        guard let parameters = parameters else { return }
-        
+        guard let parameters else { return }
+
         guard let jsonData = try? JSONSerialization.data(withJSONObject: parameters) else {
             throw InternalError.serializationFailure
         }
-        
+
         request.httpBody = jsonData
     }
 }
