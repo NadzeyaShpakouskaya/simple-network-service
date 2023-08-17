ca83703f94f9cef74c1a43b45c8d8339217142b1
NadzeyaShpakouskaya <shpakovna@gmail.com>
2023-08-17 14:02:29 +0400

    Fix BodyEncoder: improve doc comments

--- Sources/NetworkService/BodyEncoder/BodyEncoder.swift
+++ Sources/NetworkService/BodyEncoder/BodyEncoder.swift
@@ -3,13 +3,12 @@ import Foundation
 /// A protocol for encoding body parameters into an `URLRequest`.
 public protocol BodyEncoder {
     /**
-     Encodes body parameters (such as JSON, URL-encoded form data, etc.) into the provided `URLRequest`.
+     Encodes body parameters (such as JSON, URL-encoded form data, etc.) into the provided request.
 
      - Parameters:
-        - parameters: An optional `BodyParameters` object containing the data to be encoded.
-        - request: An `inout` `URLRequest` object to which the encoded body will be assigned.
-
-     - Throws: Implementations of this method may throw errors if there are issues during the encoding process.
+        - parameters: An optional set of parameters that contains the data to encode.
+        - request: A request object in which provided parameters will be encoded.
+     - Throws: A conforming implementations may throw errors if there are issues during the encoding process.
      */
     static func encode(_ parameters: BodyParameters?, into request: inout URLRequest) throws
 }
