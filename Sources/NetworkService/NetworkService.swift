47f42bfe8243951aa4a66fb051fafda289c3a477
NadzeyaShpakouskaya <shpakovna@gmail.com>
2023-08-17 13:43:27 +0400

    Fixed: remove changes that are not related the current PR

--- Sources/NetworkService/NetworkService.swift
+++ Sources/NetworkService/NetworkService.swift
@@ -1,8 +1,8 @@
 /// A stub object for future use as a network layer.
-public final class NetworkService {
+final public class NetworkService {
     /// Initialize the new instance of `NetworkService`
     public init() {}
-
+    
     /// A test method that does nothing. You can use the current method to check the availability of the API in your module.
     public func testUse() {}
 }
