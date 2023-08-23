476358d827352d5ba068f9722dba7f01739c25b6
kirpichonok <ivankirpichonok@gmail.com>
2023-08-23 10:26:29 +0300

    Update ParametersEncoder: fix formatting of documentation.

--- Sources/NetworkService/ParametersEncoder/ParametersEncoder.swift
+++ Sources/NetworkService/ParametersEncoder/ParametersEncoder.swift
@@ -5,7 +5,7 @@ public protocol ParametersEncoder {
     /// Encodes provided set of parameters as URL parameters into the given request's URL.
     ///
     /// - Parameters:
-    ///  - parameters: An optional set of URL parameters to be encoded.
-    ///  - request: A request object to which URL the encoded parameters will be assigned.
+    ///   - parameters: An optional set of URL parameters to be encoded.
+    ///   - request: A request object to which URL the encoded parameters will be assigned.
     static func encode(_ parameters: URLParameters?, into request: inout URLRequest)
 }
