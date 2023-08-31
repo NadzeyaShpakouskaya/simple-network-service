f3d129cc4d81e6951b2343348622a80dc57faef7
Alex Zavadskiy <103530508+iBYarchitect@users.noreply.github.com>
2023-08-31 13:27:28 +0200

    Adding doc comment for MockEndpoint

--- Tests/NetworkServiceTests/MockEndpoint.swift
+++ Tests/NetworkServiceTests/MockEndpoint.swift
@@ -1,6 +1,7 @@
 import NetworkService
 import Foundation
 
+/// Represents a mock HTTP API endpoint for testing purposes.
 struct MockEndpoint: HTTPAPIEndpoint {
     static var networkEnvironment: String = "test"
     var baseURL: URL = .init(string: "https://api.example.com")!
